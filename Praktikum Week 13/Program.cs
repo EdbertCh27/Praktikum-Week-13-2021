2a76e17e2cdeb8f667cdcc1d5ddc0825b72ee7e1
EdbertCh27 <edbertch27@gmail.com>
2021-12-06 09:05:38 +0800

    Add project files.

---
+++ Praktikum Week 13/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Praktikum_Week_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
